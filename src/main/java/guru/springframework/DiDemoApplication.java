2467251d57fa120e2384fd007b8dde8e8303f419
edwizzy102 <eddieekpo1992@Live.com>
2018-07-18 19:41:21 -0700

    finished Profile and Qualifier section

--- src/main/java/guru/springframework/DiDemoApplication.java
+++ src/main/java/guru/springframework/DiDemoApplication.java
@@ -14,7 +14,7 @@ public class DiDemoApplication {
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
 
-		MyController controller = (MyController) ctx.getBean("myController");
+        MyController controller = (MyController) ctx.getBean(MyController.class);
 
 		System.out.println(controller.hello());
 		System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
--- src/main/java/guru/springframework/services/PrimaryGermanGreetingService.java
+++ src/main/java/guru/springframework/services/PrimaryGermanGreetingService.java
@@ -5,15 +5,14 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 /**
- * Created by jt on 5/24/17.
+ * Created by Eddie Ekpo on 7/15/2018
  */
 @Service
 @Primary
-@Profile({"en", "default"})
-public class PrimaryGreetingService implements GreetingService {
-
+@Profile("de")
+public class PrimaryGermanGreetingService implements GreetingService {
     @Override
     public String sayGreeting() {
-        return "Hello - Primary Greeting service";
+        return "Primärer Begrüßungs Dienst";
     }
 }
